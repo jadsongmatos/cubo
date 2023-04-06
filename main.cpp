b3eaa24295f10b0e6e214a9302279edd97b8550f
Jadson G. Matos <50336793+jadsongmatos@users.noreply.github.com>
2023-04-06 03:17:21 +0000

    2

--- main.cpp
+++ main.cpp
@@ -1,126 +1,79 @@
 #include <iostream>
-#include <vulkan/vulkan.h>
+#include <cstdlib>
 #include <wayland-client.h>
+#include <vulkan/vulkan.h>
 
-// Declare Wayland objects
-struct wl_display *display;
-struct wl_compositor *compositor;
-struct wl_shell *shell;
-struct wl_surface *surface;
-struct wl_shell_surface *shell_surface;
-
-// Declare Vulkan objects
-VkInstance instance;
-VkPhysicalDevice physicalDevice;
-VkDevice device;
-
-// Wayland registry global callback
-static void registry_global_callback(void *data, struct wl_registry *registry, uint32_t name, const char *interface, uint32_t version) {
-    if (strcmp(interface, "wl_compositor") == 0) {
-        compositor = (wl_compositor *)wl_registry_bind(registry, name, &wl_compositor_interface, 1);
-    } else if (strcmp(interface, "wl_shell") == 0) {
-        shell = (wl_shell *)wl_registry_bind(registry, name, &wl_shell_interface, 1);
-    }
+static void registry_global(void *data, struct wl_registry *registry, uint32_t name,
+                            const char *interface, uint32_t version) {
+  auto app = reinterpret_cast<wayland_data *>(data);
+  if (strcmp(interface, "wl_compositor") == 0) {
+    app->compositor = static_cast<wl_compositor *>(wl_registry_bind(registry, name,
+                                                &wl_compositor_interface, 1));
+  } else if (strcmp(interface, "wl_shell") == 0) {
+    app->shell = static_cast<wl_shell *>(wl_registry_bind(registry, name,
+                                            &wl_shell_interface, 1));
+  }
 }
 
-// Wayland registry global remove callback (empty, required by Wayland)
-static void registry_global_remove_callback(void *data, struct wl_registry *registry, uint32_t name) {
-}
-
-// Create a Wayland window
-void create_wayland_window() {
-    // Initialize Wayland display and registry
-    display = wl_display_connect(NULL);
-    struct wl_registry *registry = wl_display_get_registry(display);
-
-    static const struct wl_registry_listener registry_listener = {
-        .global = registry_global_callback,
-        .global_remove = registry_global_remove_callback
-    };
-
-    wl_registry_add_listener(registry, &registry_listener, NULL);
-    wl_display_roundtrip(display);
-
-    // Create Wayland compositor and shell
-    surface = wl_compositor_create_surface(compositor);
-    shell_surface = wl_shell_get_shell_surface(shell, surface);
-
-    // Set Wayland window properties
-    wl_shell_surface_set_toplevel(shell_surface);
-}
-
-// Initialize Vulkan instance
-void init_vulkan() {
-    VkApplicationInfo appInfo = {};
-    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-    appInfo.pApplicationName = "VulkanWaylandExample";
-    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.pEngineName = "No Engine";
-    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.apiVersion = VK_API_VERSION_1_0;
-
-    VkInstanceCreateInfo createInfo = {};
-    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-    createInfo.pApplicationInfo = &appInfo;
-
-    // Enable necessary Vulkan extensions
-    const char *extensions[] = {
-        "VK_KHR_surface",
-        "VK_KHR_wayland_surface"
-    };
-
-    createInfo.enabledExtensionCount = sizeof(extensions) / sizeof(extensions[0]);
-    createInfo.ppEnabledExtensionNames = extensions;
-
-    // Create Vulkan instance
-    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
-        std::cerr << "Failed to create Vulkan instance!" << std::endl;
-        exit(1);
-    }
-}
+static const struct wl_registry_listener registry_listener = {
+    registry_global,
+    nullptr,
+};
 
 int main() {
-    // Create a Wayland window
-    create_wayland_window();
+  // Initialize Wayland
+  wl_display *display = wl_display_connect(nullptr);
+  if (display == nullptr) {
+    std::cerr << "Failed to connect to Wayland display" << std::endl;
+    return 1;
+  }
 
-    // Initialize Vulkan
-    init_vulkan();
+  wl_registry *registry = wl_display_get_registry(display);
+  wayland_data data;
+  wl_registry_add_listener(registry, &registry_listener, &data);
+  wl_display_roundtrip(display);
 
-    // (Insert code to create a Vulkan surface, swapchain, render pass, pipeline, framebuffers, and command buffers)
+  if (data.compositor == nullptr || data.shell == nullptr) {
+    std::cerr << "Failed to find required Wayland interfaces" << std::endl;
+    return 1;
+  }
 
-    // (Insert code to create a 3D cube, set
-// (Insert code to implement transformation and projection matrix functions)
+  wl_surface *surface = wl_compositor_create_surface(data.compositor);
+  wl_shell_surface *shell_surface = wl_shell_get_shell_surface(data.shell, surface);
+  wl_shell_surface_set_toplevel(shell_surface);
 
-// Main loop
-while (true) {
-    // Handle Wayland events
+  // Initialize Vulkan
+  VkApplicationInfo app_info = {};
+  app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+  app_info.pApplicationName = "Vulkan Wayland Example";
+  app_info.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+  app_info.pEngineName = "No Engine";
+  app_info.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+  app_info.apiVersion = VK_API_VERSION_1_0;
+
+  VkInstanceCreateInfo create_info = {};
+  create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+  create_info.pApplicationInfo = &app_info;
+
+  VkInstance instance;
+  if (vkCreateInstance(&create_info, nullptr, &instance) != VK_SUCCESS) {
+    std::cerr << "Failed to create Vulkan instance" << std::endl;
+    return 1;
+  }
+
+  // Main loop
+  while (true) {
     wl_display_dispatch_pending(display);
+  }
 
-    // (Insert code to handle input and window events)
+  // Cleanup
+  vkDestroyInstance(instance, nullptr);
+  wl_shell_surface_destroy(shell_surface);
+  wl_surface_destroy(surface);
+  wl_shell_destroy(data.shell);
+  wl_compositor_destroy(data.compositor);
+  wl_registry_destroy(registry);
+  wl_display_disconnect(display);
 
-    // For each frame, acquire an image from the swapchain
-    // (Insert code to acquire an image from the swapchain)
-
-    // Submit rendering commands to the image using a graphics queue
-    // (Insert code to record and submit command buffers for rendering)
-
-    // Present the rendered image in the window using a presentation queue
-    // (Insert code to present the image using a presentation queue)
-
-    // Synchronize rendering and presentation using semaphores and/or fences
-    // (Insert code to synchronize rendering and presentation)
-}
-
-// Cleanup and shutdown
-// (Insert code to release Vulkan resources and destroy created objects)
-
-// Terminate the Wayland library and close the window
-wl_shell_surface_destroy(shell_surface);
-wl_surface_destroy(surface);
-wl_shell_destroy(shell);
-wl_compositor_destroy(compositor);
-wl_registry_destroy(registry);
-wl_display_disconnect(display);
-
-return 0;
+  return 0;
 }
